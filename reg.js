1a43a094ed3b47ad49f75879101d51185b48e213
Pallavinehaaa23 <114822231+Pallavinehaaa23@users.noreply.github.com>
2023-02-10 01:29:29 +0530

    Update reg.js

--- reg.js
+++ reg.js
@@ -1,6 +1,11 @@
 const but=document.getElementById('butn');
 
 but.addEventListener('click',function(){
-   let x=document.getElementById('regform').innerText;
-   return x;
+   document.write(`<div class="regform" id="botn">
+   <a>FILL UPS</a>
+   Are you a donor?<input type="radio"><br>
+   Are you a reciever?<input type="radio"><br>
+   Name<input type="text" placeholder="Enter Your NAME">
+</div>
+`)
 })
